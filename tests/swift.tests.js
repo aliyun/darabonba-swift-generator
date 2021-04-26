88aa5b63fa91e5192925fe95c526b3b851faee1d
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2021-04-26 10:33:14 +0800

    ci: patch yml

--- tests/swift.tests.js
+++ tests/swift.tests.js
@@ -2,14 +2,11 @@
 
 const path = require('path');
 const fs = require('fs');
+const promisify = require('util').promisify;
+const exists = promisify(fs.exists);
+const readFile = promisify(fs.readFile);
 const assert = require('assert');
 require('mocha-sinon');
-
-const {
-  _name,
-  _camelCase,
-  _upperFirst
-} = require('../src/lib/helper');
 
 const DSL = require('@darabonba/parser');
 const Generator = require('../src/generator');
@@ -20,13 +17,18 @@ const expectedDir = path.join(__dirname, 'expected/');
 const fixturesDir = path.join(__dirname, 'fixtures/');
 const outputDir = path.join(__dirname, '../', 'output/tests/');
 
-function check(moduleName, expectedFiles = []) {
+async function check(moduleName, expectedFiles = []) {
   const mainFilePath = path.join(fixturesDir, moduleName, 'main.dara');
   const moduleOutputDir = path.join(outputDir, moduleName);
   const prefixDir = path.join(fixturesDir, moduleName);
-  const pkgContent = fs.readFileSync(
-    fs.existsSync(path.join(prefixDir, 'Darafile')) ?
-      path.join(prefixDir, 'Darafile') : path.join(prefixDir, 'Teafile'), 'utf8');
+  let pkgContent = '';
+  if (await exists(path.join(prefixDir, 'Darafile'))) {
+    pkgContent = await readFile(path.join(prefixDir, 'Darafile'), 'utf-8');
+  } else if (await exists(path.join(prefixDir, 'Teafile'))) {
+    pkgContent = await readFile(path.join(prefixDir, 'Teafile'), 'utf-8');
+  } else {
+    throw new Error(`Darafile or Teafile not exist in ${prefixDir}`);
+  }
   const pkgInfo = JSON.parse(pkgContent);
   const config = {
     outputDir: moduleOutputDir,
@@ -38,15 +40,12 @@ function check(moduleName, expectedFiles = []) {
   const dsl = fs.readFileSync(mainFilePath, 'utf8');
   const ast = DSL.parse(dsl, mainFilePath);
   generator.visit(ast);
-  let package_name = `${_upperFirst(_camelCase(_name(config.scope)))}_${_upperFirst(_camelCase(_name(config.name)))}`;
-  setTimeout(function () {
-    expectedFiles.forEach(element => {
-      const outputFilePath = path.join(outputDir, moduleName, element);
-      const expectedFilePath = path.join(expectedDir, moduleName, element);
-      const expected = fs.readFileSync(expectedFilePath, 'utf8');
-      assert.deepStrictEqual(fs.readFileSync(outputFilePath, 'utf8'), expected);
-    });
-  }, 2000);
+  expectedFiles.forEach(element => {
+    const outputFilePath = path.join(outputDir, moduleName, element);
+    const expectedFilePath = path.join(expectedDir, moduleName, element);
+    const expected = fs.readFileSync(expectedFilePath, 'utf8');
+    assert.deepStrictEqual(fs.readFileSync(outputFilePath, 'utf8'), expected);
+  });
 }
 
 describe('Swift Generator', function () {
